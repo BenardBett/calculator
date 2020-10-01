8d0e142f94f4194656c39df17a615d967d9f29ba
BenardBett <kiplangatbett5@gmail.com>
2020-10-01 11:15:40 +0300

    modified script.js file

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,42 @@ var add = function(number1, number2) {
   var number2 = parseInt(prompt("Enter another number:"));
   var result = add(number1, number2);
   alert(result);
+  
+  var bodyMass=function(weight,height){
+    return weight/height;
+  };
+  var weight=prompt("Enter the weight of the person in kg.");
+  var height=prompt("Enter the height of the person in metres.");
+  var result=bodyMass(weight/height);
+  alert(result);
+
+  var celcius= function(tempCelcius, conversion){
+  return  tempCelcius * conversion;
+}
+var tempCelcius=prompt("Enter the temperature in degrees celsius");
+var conversion=prompt("Enter the conversion formulae");
+var result= tempCelcius * conversion;
+alert(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
